b81aa7b162f7e242ae44ef62ab5e43245b40d394
王超 <597584896@qq.com>
2019-06-12 18:01:08 +0800

    add center

--- src/pages/center/center.vue
+++ src/pages/center/center.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="center-container">
+    <div class="center-info">
+
+    </div>
     <div v-if="role==='user'">
       个人中心嘎嘎嘎嘎嘎12
-
     </div>
     <div v-else>
 
@@ -24,6 +26,17 @@ export default {
 }
 </script>
 <style lang="less">
+.center-container {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  .center-info {
+    width: 100%;
+    height: 200rpx;
+    background-color: #339999;
+  }
+}
 </style>
 
 
