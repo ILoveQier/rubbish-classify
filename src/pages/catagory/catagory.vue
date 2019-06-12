840dd21555005c6bface370c2fb705b79efe4819
王超 <597584896@qq.com>
2019-06-12 17:29:35 +0800

    add catagory

---
+++ src/pages/catagory/catagory.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="catagory-container">
+    <div class="left">
+      <div class="left-wrap">
+        <span v-for="(item,i) in items"
+              @click="index=i"
+              :style="{'backgroundColor':index===i?'#fff':'#e5e5e5'}">{{item}}</span>
+      </div>
+    </div>
+    <div class="right">
+      <div class="right-wrap">
+        <span v-for="item in itemsDetails">{{item}}</span>
+        <span></span>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      index: 0,
+      items: ['家电', '纸张', '塑料'],
+      itemsDetails: ['废旧吹风机', '废旧电冰箱', '废旧电风扇', '废旧吹风机', '废旧彩电', '废旧电风扇', '废旧吹风机', '废旧电冰箱', '废旧电风扇', '废旧冰箱', '废旧空调']
+    }
+  },
+}
+</script>
+<style lang="less">
+.catagory-container {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  .left {
+    height: 100%;
+    width: 30%;
+    background-color: #e5e5e5;
+    .left-wrap {
+      display: flex;
+      flex-direction: column;
+      span {
+        padding: 30rpx;
+        text-align: center;
+      }
+    }
+  }
+  .right {
+    flex: 1;
+    .right-wrap {
+      padding-top: 50rpx;
+      display: flex;
+      justify-content: space-between;
+      flex-wrap: wrap;
+      span {
+        width: 150rpx;
+        text-align: center;
+        font-size: 30rpx;
+        margin-bottom: 50rpx;
+      }
+    }
+  }
+}
+</style>
+
+
---
+++ src/pages/catagory/main.js
@@ -0,0 +1,6 @@
+import Vue from 'vue'
+import Catagory from './catagory.vue'
+
+const catagory = new Vue(Catagory)
+
+catagory.$mount() // 挂在当前页面
